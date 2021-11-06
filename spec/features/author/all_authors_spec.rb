9b43609d688f315071a1548333c3971dcac6563d
Finn-HPI <finn.schoellkopf@student.hpi.uni-potsdam.de>
2021-11-06 10:01:57 +0100

    add test for show detail link of authors

--- spec/features/author/all_authors_spec.rb
+++ spec/features/author/all_authors_spec.rb
@@ -11,4 +11,10 @@ describe "All authors page", type: :feature do
         visit authors_path
         expect(page).to have_link 'New', href: new_author_path
     end
+    it "should have an author 'Alan Turing' with a link to the author's details" do
+        Author.create(first_name: 'Alan', last_name: 'Turing', homepage: 'http://wikipedia.org/Alan_Turing')
+        visit authors_path
+        expect(page).to have_selector 'td', text: 'Alan Turing'
+        expect(page).to have_link 'Show'
+    end
 end
